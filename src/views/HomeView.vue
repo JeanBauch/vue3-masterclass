2b23b9b814016fb1c6a74e5802bd1650aed7e318
Jean Bauch <jean.bauch06@gmail.com>
2023-02-20 13:29:24 -0300

    feat: Chapter-2-done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup>
-import TheWelcome from "../components/TheWelcome.vue";
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <PageHome />
   </main>
 </template>
+
+<script setup>
+import PageHome from "@/components/PageHome.vue";
+</script>
